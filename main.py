f3954013d2d8a360149854d04e37d0ad422bf7fa
Bohdan Mazur <112277633+Mr-Vulpes@users.noreply.github.com>
2023-12-20 18:34:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from packaging import version
 
 required_version = version.parse("1.1.1")
 current_version = version.parse(openai.__version__)
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 if current_version < required_version:
   raise ValueError(
       f"Error: OpenAI version {openai.__version__} is less than the required version 1.1.1"
